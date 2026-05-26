3707eda455c6c7f01a54393646883189d730029c
Isaac Obo Enimil <isaac.enimil@amalitech.com>
2026-05-26 08:07:34 +0000

    Add Terraform configurations for S3 bucket, including tags and variables for resource tagging

--- terraform/00-bootstrape-bucket/main.tf
+++ terraform/00-bootstrape-bucket/main.tf
@@ -1,6 +1,8 @@
 resource "aws_s3_bucket" "terraform_state" {
   bucket = var.bucket_name
 
+  tags = var.tags
+
   lifecycle {
     prevent_destroy = true
   }
--- terraform/00-bootstrape-bucket/variables.tf
+++ terraform/00-bootstrape-bucket/variables.tf
@@ -8,3 +8,13 @@ variable "bucket_name" {
   description = "The name of the S3 bucket. Must be globally unique."
   type        = string
 }
+
+variable "tags" {
+  description = "Tags to apply to resources"
+  type        = map(string)
+  default = {
+    Environment = "Audit"
+    Project     = "CostDetective"
+    ManagedBy   = "Terraform"
+  }
+}
